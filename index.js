ca7e19610914ec56c8d8b5e5ebd94de0ba46ccbe
Denis Valdivieso <sshshaka@gmail.com>
2022-06-10 17:44:01 -0400

    Basic server, EcmaScript modules, reading env variables (dotenv)

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import 'dotenv/config'
+
+const app = express()
+
+app.get('/', (req, res) => res.send('<h1>Testing Server</h1>'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
