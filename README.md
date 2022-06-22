9a79de4a2538ef9c46edde3e87801a00704a34c9
Denis Valdivieso <sshshaka@gmail.com>
2022-06-21 20:12:27 -0400

    cors script "start" - heroku pre-deploy

---
+++ README.md
@@ -0,0 +1,3 @@
+# API REST
+
+## Urls Shortener
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import 'dotenv/config'
-import express from 'express'
 import './database/connect.js'
+import cors from 'cors'
+import express from 'express'
 import authRouter from './routes/auth.js'
 import linkRouter from './routes/link.js'
 import redirectRouter from './routes/redirect.js'
 import cookieParser from 'cookie-parser'
 
 const app = express()
+
+const whiteList = [process.env.ORIGIN_REACT, process.env.ORIGIN_VUE]
+app.use(
+  cors({
+    origin: function (origin, cb) {
+      if (!origin || whiteList.includes(origin)) return cb(null, origin)
+
+      return cb('Cors Error: Unauthorized Origin')
+    }
+  })
+)
+
 app.use(express.json())
 app.use(express.static('public'))
 app.use(cookieParser())
